81c85d1e7a494c88831b9b73955199f138546bdf
MohammadBorabadi <MohammadBorabadi0@gmail.com>
2022-02-10 09:50:46 -0800

    Create TodoItem

--- src/Components/TodoList/TodoList.js
+++ src/Components/TodoList/TodoList.js
@@ -2,6 +2,7 @@ import React from "react";
 
 // Context
 import { useTodo } from "../../Provider/TodoProvider";
+import TodoItem from "../TodoItem";
 
 // Css
 import styles from "./TodoList.module.css";
@@ -9,7 +10,13 @@ import styles from "./TodoList.module.css";
 const TodoList = () => {
   const { todos } = useTodo();
 
-  return <div className={styles.todoItem}></div>;
+  return (
+    <div className={styles.todoList}>
+      {todos.map((item) => (
+        <TodoItem item={item} key={item.id} />
+      ))}
+    </div>
+  );
 };
 
 export default TodoList;
